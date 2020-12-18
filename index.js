70f27de4e720494816b86b2a979ff84906c419cd
lei2rock <zjpjzdl@qq.com>
2020-12-18 16:35:03 +0800

    Create Friends Repo

---
+++ index.js
@@ -0,0 +1,15 @@
+const yaml = require('js-yaml');
+const { readFileSync, mkdirSync, writeFileSync } = require('fs');
+
+// Get document, or throw exception on error
+try {
+    const doc = yaml.safeLoad(readFileSync('./src/friendslist.yml', 'utf8'));
+    try {
+        mkdirSync('./dist/');
+    } catch ({ code }) {
+        if (code !== 'EEXIST') return;
+    }
+    writeFileSync('./dist/friendslist.json', JSON.stringify(doc));
+} catch (e) {
+    console.error(e);
+}
